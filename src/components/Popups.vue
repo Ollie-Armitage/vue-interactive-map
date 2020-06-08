69f6ced2e5ea50165d45e4a5f63c717c7f979273
Ollie Armitage <oliverrarmitage@gmail.com>
2020-06-08 11:12:56 +0100

    No need for login anymore.

--- src/components/Popups.vue
+++ src/components/Popups.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
-    <Login @closeWindow="closeLoginWindow" v-if="this.$store.getters.getLoginWindowOpen === true"></Login>
     <Settings @closeWindow="closeSettingsWindow" v-if="this.$store.getters.getSettingsWindowOpen === true"></Settings>
   </div>
 </template>
 
 <script>
-import Login from './Login'
 import Settings from './Settings'
 
 export default {
   name: 'Popups',
   components: {
-    Login,
     Settings
   },
   methods: {
